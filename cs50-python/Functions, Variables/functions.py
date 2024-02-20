c8847906a6ef0042974baab8c391d7595801c79d
skitts4code <skitts4code@gmail.com>
2024-02-20 21:54:27 +0300

    add 1st course

---
+++ cs50-python/Functions, Variables/functions.py
@@ -0,0 +1,19 @@
+def main():
+    name = input("What is Your name? ")
+    hello(name)
+
+def hello(to="World"):
+    print("hello,", to)
+
+main()
+hello()
+
+
+
+# Alt Method
+'''
+def hello(to="World"):
+    print("hello,", to)
+hello()
+'''
+
