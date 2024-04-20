b8402deacaa0c42cb7559d41c9df53ba39cffebb
skitts4code <skitts4code@gmail.com>
2024-04-20 04:54:56 +0300

    Damn this made me think

--- coding practice/ObjectOrientedProgramming/main.py
+++ coding practice/ObjectOrientedProgramming/main.py
@@ -1,13 +1,24 @@
-# from turtle import Turtle, Screen
-#
-# timmy = Turtle()
-#
-# timmy.shape("turtle")
-# timmy.color("chartreuse")
-# timmy.forward(100)
-#
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+menu_items = menu.get_items()
+
+is_on = True
+
+while is_on:
+    answer = input(f"What would you like? {menu_items} ")
+    if answer == "off":
+        is_on = False
+        print("Bye")
+    elif answer == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(answer)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
